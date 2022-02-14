80008a7dd1f3c5285666d8b583a9f067b53afc8f
Kunanon <fronkzoza001@gmail.com>
2022-02-14 13:36:23 +0700

    Update

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -21,9 +21,11 @@ contract MyEpicNFT is ERC721URIStorage {
         // Actually mint the NFT to the sender using msg.sender.
         _safeMint(msg.sender, newItemId);
         // Set the NFTs data.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/JURE");
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UiCn0=");
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
         // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();
     }
 }
+
+//https://rinkeby.rarible.com/token/INSERT_DEPLOY_CONTRACT_ADDRESS_HERE:INSERT_TOKEN_ID_HERE
